7b0da067f661be1f287df98590a27e189d6ac5ae
Andrii Koliada <andykoliada@gmail.com>
2020-11-18 15:51:53 +0200

    adding docker support

--- AddressBook/Models/AddressBook.cs
+++ AddressBook/Models/AddressBook.cs
@@ -7,5 +7,20 @@ namespace AddressBook.Models
 {
     public class AddressBook
     {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Display(Name = "Модель")]
+        public string Name { get; set; }
+        [Display(Name = "Производитель")]
+        public string Company { get; set; }
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+
+        public string ImageId { get; set; } // ссылка на изображение
+
+        public bool HasImage()
+        {
+            return !String.IsNullOrWhiteSpace(ImageId);
+        }
     }
 }
